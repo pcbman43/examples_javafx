6e9d3bfc8b91d110f68e22937ebd3a290f3cb764
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-04-11 10:56:53 +0300

    JAVA FX töölehed - 1.2 Stage klass

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -3,27 +3,18 @@ package ee.khk;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Launching application");
         Application.launch(args);
     }
 
     @Override
-    public void init() throws Exception {
-        System.out.println("Application inits");
-        super.init();
-    }
-
-    @Override
     public void start(Stage stage) {
-        System.out.println("Application starts");
-        stage.show();
+    stage.setTitle("Hello JavaFX!");
+    stage.setWidth(250);
+    stage.setHeight(150);
+    stage.show();
     }
 
-    @Override
-    public void stop() throws Exception {
-        System.out.println("Application stops");
-        super.stop();
-    }
 }
