a57a864430b76f7c22d8b267d8c8b35cf0b25667
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-04-25 18:58:31 +0300

    JAVA FX töölehed - 1.5 Kasutajaga koostöö ja sündmuste juhtimine

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,11 +1,12 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Group;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class Main extends Application {
 
@@ -16,12 +17,23 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Group root = new Group();
-        Scene scene = new Scene(root, 400, 150, Color.BLUE);
-        // taustavärvi määramise alternatiiv: scene.setFill(color.BLUE);
+        Button btn = new Button();
+        btn.setText("Click!");
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                btn.setText("You've clicked!");
+            }
+        });
+
+        Group root = new Group(btn);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
 
         stage.setTitle("Hello JavaFX!");
+        stage.setWidth(250);
+        stage.setHeight(200);
 
         stage.show();
     }
