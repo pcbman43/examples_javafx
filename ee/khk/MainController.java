b50bb807d4d0d0d05c2e1bd31de2432211e859ca
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-04-26 20:19:57 +0300

    JAVA FX töölehed - 2.2 Kontrollerid ja koostöö kasutajaga

---
+++ ee/khk/MainController.java
@@ -0,0 +1,22 @@
+package ee.khk;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private void click(ActionEvent event) {
+        btn.setText("You've clicked!");
+    }
+
+
+
+
+
+
+}
