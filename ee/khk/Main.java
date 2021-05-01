76352a37881c0112f7283fb928ed3d75989811c8
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-01 22:48:14 +0300

    JAVA FX töölehed - 3.2(.3) FlowPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,6 +1,7 @@
 package ee.khk;
 
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -20,19 +21,9 @@ public class Main extends Application {
 
         Label label1 = new Label("Label1");
         Label label2 = new Label("Label2");
-        Label label3 = new Label("Label3");
-        Label label4 = new Label("Label4");
-        Label label5 = new Label("Label5");
-        Label label6 = new Label("Label6");
-        Label label7 = new Label("Label7");
-        Label label8 = new Label("Label8");
-        Label label9 = new Label("Label9");
 
-        FlowPane root = new FlowPane();
-        root.setVgap(18);
-        root.setHgap(15);
-        root.setOrientation(Orientation.VERTICAL);
-        root.getChildren().addAll(label1, label2, label3, label4, label5, label6, label7, label8, label9);
+        FlowPane root = new FlowPane(10, 10, label1, label2);
+        root.setAlignment(Pos.CENTER);
         Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
 
