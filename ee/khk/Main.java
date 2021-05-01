bbcef9e08774ac930b85a0c9d4e81a3ec962b25f
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-01 22:38:09 +0300

    JAVA FX töölehed - 3.2 FlowPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,10 +1,8 @@
 package ee.khk;
 
-import javafx.scene.control.Button;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -20,16 +18,23 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Label lbl = new Label("Hello");
-        Button btn = new Button("Click");
+        Label label1 = new Label("Label1");
+        Label label2 = new Label("Label2");
+        Label label3 = new Label("Label3");
+        Label label4 = new Label("Label4");
+        Label label5 = new Label("Label5");
+        Label label6 = new Label("Label6");
+        Label label7 = new Label("Label7");
+        Label label8 = new Label("Label8");
+        Label label9 = new Label("Label9");
 
-        FlowPane root = new FlowPane();
-        root.getChildren().addAll(lbl, btn);
-        Scene scene = new Scene(root, 300, 200);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, label1, label2, label3, label4, label5, label6, label7, label8, label9);
+        //root.getChildren().addAll(label1, label2, label3, label4, label5, label6, label7, label8, label9);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
 
-        stage.setTitle("Hello JavaFX!");
-        stage.setWidth(250);
+        stage.setTitle("FlowPane in JavaFX");
+        stage.setWidth(300);
         stage.setHeight(200);
 
         stage.show();
