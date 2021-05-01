8c8e3c1d24db5c6a161a04e73f00d764a47392bc
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-01 21:16:11 +0300

    JAVA FX töölehed - 3.1 Paigutuspaneelide tüübid

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,10 +1,12 @@
 package ee.khk;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,11 +18,14 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(root);
+        Label lbl = new Label("Hello");
+        Button btn = new Button("Click");
 
+        FlowPane root = new FlowPane();
+        root.getChildren().addAll(lbl, btn);
+        Scene scene = new Scene(root, 300, 200);
         stage.setScene(scene);
 
         stage.setTitle("Hello JavaFX!");
