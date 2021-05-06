3ea04db31f4257950651789a9df3a9a020c41297
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-07 01:14:24 +0300

    JAVA FX töölehed - 3.6(.3) StackPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -2,6 +2,7 @@ package ee.khk;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
@@ -22,8 +23,12 @@ public class Main extends Application {
 
         Label headerLbl = new Label("Press the login button");
         StackPane.setAlignment(headerLbl, Pos.TOP_CENTER);
+        StackPane.setMargin(headerLbl, new Insets(10, 0, 0, 0));
+
         Label statusLbl = new Label("Start screen");
         StackPane.setAlignment(statusLbl, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(statusLbl, new Insets(0, 0, 10, 0));
+
         Button loginBtn = new Button("Login");
 
         StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
