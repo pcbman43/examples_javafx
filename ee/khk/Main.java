cb3c0e86357f9100c459dbccd3c59181aa82dc0e
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-07 01:09:19 +0300

    JAVA FX töölehed - 3.6(.1) StackPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,9 +1,9 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
@@ -19,19 +19,16 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        BorderPane root = new BorderPane();
+        Label headerLbl = new Label("Press the login button");
+        Label statusLbl = new Label("Start screen");
+        Button loginBtn = new Button("Login");
 
-        Button top = new Button("Top");
-        BorderPane.setAlignment(top, Pos.CENTER);
-        root.setTop(top);
-
-        Button center = new Button("Center");
-        root.setCenter(center);
+        StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
 
         Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
 
-        stage.setTitle("BorderPane in JavaFX");
+        stage.setTitle("StackPane in JavaFX");
 
         stage.show();
     }
