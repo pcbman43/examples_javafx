f91f3d01a5278c7dd3d265eb06e74ae05dcadfd0
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-05-07 01:28:06 +0300

    JAVA FX töölehed - 3.6 AnchorPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -21,22 +22,32 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Label headerLbl = new Label("Press the login button");
-        StackPane.setAlignment(headerLbl, Pos.TOP_CENTER);
-        StackPane.setMargin(headerLbl, new Insets(10, 0, 0, 0));
+        Button topBtn = new Button("Top");
+        AnchorPane.setTopAnchor(topBtn, 10.0);
+        AnchorPane.setLeftAnchor(topBtn, 60.0);
+        AnchorPane.setRightAnchor(topBtn, 60.0);
 
-        Label statusLbl = new Label("Start screen");
-        StackPane.setAlignment(statusLbl, Pos.BOTTOM_CENTER);
-        StackPane.setMargin(statusLbl, new Insets(0, 0, 10, 0));
+        Button bottomBtn = new Button("Bottom");
+        AnchorPane.setBottomAnchor(bottomBtn, 10.0);
+        AnchorPane.setLeftAnchor(bottomBtn, 60.0);
+        AnchorPane.setRightAnchor(bottomBtn, 60.0);
 
-        Button loginBtn = new Button("Login");
+        Button leftBtn = new Button("Left");
+        AnchorPane.setTopAnchor(leftBtn, 30.0);
+        AnchorPane.setLeftAnchor(leftBtn, 15.0);
+        AnchorPane.setBottomAnchor(leftBtn, 30.0);
 
-        StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
+        Button rightBtn = new Button("Right");
+        AnchorPane.setTopAnchor(rightBtn, 30.0);
+        AnchorPane.setRightAnchor(rightBtn, 10.0);
+        AnchorPane.setBottomAnchor(rightBtn, 30.0);
+
+        AnchorPane root = new AnchorPane(topBtn, rightBtn, bottomBtn, leftBtn);
 
         Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
 
-        stage.setTitle("StackPane in JavaFX");
+        stage.setTitle("AnchorPane in JavaFX");
 
         stage.show();
     }
